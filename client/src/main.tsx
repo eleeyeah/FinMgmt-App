0973f5f89deeea17a8216d643e5f22d591435cae
eleeyeah <ilietmks@gmail.com>
2023-05-11 16:34:55 +0200

    server configs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
   middleware: (getDefault) => getDefault().concat(api.middleware),
 });
