03bae648f5a5bfddc9df49e58411f499278f638a
eleeyeah <ilietmks@gmail.com>
2023-06-07 11:03:54 +0200

    dep stream fixed/dockerfile updated/server deployed

--- server/index.js
+++ server/index.js
@@ -30,20 +30,20 @@ app.use("/product", productRoutes);
 app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    toString: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(async () => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
-    // await mongoose.connection.db.dropDatabase();
-    // KPI.insertMany(kpis);
-    // Product.insertMany(products);
-    // Transaction.insertMany(transactions);
-  })
-  .catch((error) => console.log(`${error} did not connect`));
+        /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis);
+        // Product.insertMany(products);
+        // Transaction.insertMany(transactions);
+    })
+    .catch((error) => console.log(`${error} did not connect`));
